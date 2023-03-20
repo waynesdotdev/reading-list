18d3f36707073a563c7b775f0f2525fd7a988a97
Wayne Fox <91109871+foxwayne1@users.noreply.github.com>
2023-03-20 11:36:11 +0000

    local json-server added and updating all crud options

--- src/App.js
+++ src/App.js
@@ -1,32 +1,49 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import BookCreate from './components/BookCreate'
 import BookList from './components/BookList'
 
 function App() {
   const [books, setBooks] = useState([])
-  console.log(books)
 
-  const createBook = title => {
-    if (!title) {
-      return alert('There is no title, please add one and try')
-    }
-    const updatedBooks = [...books, { id: books.length + 1, title: title }]
+  const fetchBooks = async () => {
+    const response = await axios.get('http://localhost:3001/books')
+
+    setBooks(response.data)
+  }
+
+  useEffect(() => {
+    fetchBooks()
+  }, [setBooks])
+
+  const createBook = async title => {
+    const response = await axios.post('http://localhost:3001/books', {
+      title: title,
+    })
+
+    const updatedBooks = [...books, response.data]
     setBooks(updatedBooks)
   }
 
-  const deleteBookById = id => {
-    const updatedBooks = books.filter(book => book.id !== id)
-    setBooks(updatedBooks)
-  }
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    })
 
-  const editBookById = (id, newTitle) => {
     const updatedBooks = books.map(book => {
       if (book.id === id) {
-        return { ...book, title: newTitle }
+        return { ...book, ...response.data }
       }
       return book
     })
 
+    setBooks(updatedBooks)
+  }
+
+  const deleteBookById = async id => {
+    await axios.delete(`http://localhost:3001/books/${id}`)
+
+    const updatedBooks = books.filter(book => book.id !== id)
     setBooks(updatedBooks)
   }
 
